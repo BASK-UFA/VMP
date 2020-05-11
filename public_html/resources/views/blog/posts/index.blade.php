af485303421f640f2dde7671dde4bb0d624951f4
Руслан <ruslan.valeev18117@gmail.com>
2020-05-11 22:25:45 +0300

    BLOG

--- public_html/resources/views/blog/posts/index.blade.php
+++ public_html/resources/views/blog/posts/index.blade.php
@@ -21,19 +21,19 @@
             @foreach($paginator as $item)
                 <div class="col-md-12 pt-2 pb-2">
 
-                        <div class="card-header"style="background-color: chocolate;font-family: 'Oswald', sans-serif;">
+                        <div class=""style="font-family: 'Oswald', sans-serif;">
                             <div>
-                                <a class="text-white" href="#">{{ $item->user->name }}</a>
-                                <span class="float-md-right d-md-inline d-block" style="color: #000">{{ $item->created_at }}</span>
+                                <a class="" style="font-size:2em;" href="#">{{ $item->user->name }}</a>
+                                <span class="float-md-right d-md-inline d-block" style=";color: #000">{{ $item->created_at }}</span>
                             </div>
-                            <h3 class="pt-2">{{ $item->title }}</h3>
+                            <a href="#" style="font-size:2em;" class=" pt-2">{{ $item->title }}</a>
                         </div>
 
-                        <div class="card-footer text-white" style="background-color: chocolate;">
-                            <img style="width: 1000px;height: 550px;" class="d-flex justify-content-center" src="{{asset('images/books.jpg')}}" alt="">
-                            <div>{{ $item->excerpt }}</div>
+                        <div >
+                            <img  class="d-flex justify-content-center h-25 w-50"  src="{{asset('images/books.jpg')}}" alt="">
+                            <div style="font-family: 'Oswald', sans-serif; font-size:1.5em">{{ $item->excerpt }}</div>
                             <div>
-                                <a class="text-white" href="#">Читать далее...</a>
+                                <button class="btn text-white" style="font-size:1.2em;background-color:chocolate; ; font-family: 'Oswald', sans-serif;" href="#">Читать далее...</button>
                             </div>
 
                     </div>
