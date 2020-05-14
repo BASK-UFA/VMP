735bf6f5280aa7386e7bde80994fad7378ed5d5e
Руслан <ruslan.valeev18117@gmail.com>
2020-05-14 21:15:21 +0300

    I'm create "Read Article"

--- public_html/resources/views/blog/posts/show.blade.php
+++ public_html/resources/views/blog/posts/show.blade.php
@@ -6,6 +6,49 @@
     {{--  Верстайте тут, в $item будет модель BlogPost                              --}}
     {{--  Для обращения к полю используйте конструкцию {{ $item->НАЗВАНИЕ-ПОЛЯ }}   --}}
     {{--  Ниже вывод всех полей модели через дебагер                                --}}
-    @dd($item->getAttributes())
+
+
+
+    <div class="carousel intro_read" data-ride="carousel">
+        <ol class="carousel-indicators">
+
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{asset($item->image)}}" class="d-block w-100  intro_read bg-secondary">
+                <div class="carousel-caption  d-md-block">
+                    <h5> {{ $item->title }}</h5>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+
+                <div>
+                        <span>
+                            {{$item->user->name}}
+                        </span>
+                    <span class="float-md-right">
+                            Создано: {{$item->created_at}}
+                        </span>
+                </div>
+
+                <div class="mt-4 "><p class="article_txt">{{($item->content_html)}}</p></div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    {{--    @dd($item->getAttributes())--}}
+
+
 
 @endsection
