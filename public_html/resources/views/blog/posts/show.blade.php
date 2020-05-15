ee0711ed26a1e453e72cd7ef93f7d8ee7dc13cf5
Руслан <ruslan.valeev18117@gmail.com>
2020-05-15 21:58:54 +0300

    Refactoring

--- public_html/resources/views/blog/posts/show.blade.php
+++ public_html/resources/views/blog/posts/show.blade.php
@@ -14,12 +14,12 @@
     <div class="carousel intro_read" data-ride="carousel">
         <ol class="carousel-indicators"></ol>
         <div class="carousel-inner">
-            <div class="carousel-item active ">
-                <img src="{{asset($item->image)}}" alt="Превью" class="d-block w-100  intro_read bg-secondary">
-                <div class="carousel-caption  d-md-block  post_name">
-                    <h2 > {{ $item->title }}</h2>
+            <div class="carousel-item active">
+                <img src="{{ asset($item->image) }}" alt="Превью" class="d-block w-100 intro_read bg-secondary">
+                <div class="carousel-caption d-md-block post_name">
+                    <h2> {{ $item->title }}</h2>
                     <a href="#article_user">
-                        <div class="arrow arrow-bottom " id="article_user"></div>
+                        <div class="arrow arrow-bottom" id="article_user"></div>
                     </a>
                 </div>
             </div>
@@ -29,17 +29,17 @@
         <div class="row">
             <div class="col-12">
                 <div>
-                    <span >
+                    <span>
                         <img class="img-fluid article_avatar" src="{{ asset($item->user->avatar) }}" alt="Аватарка">
                     </span>
                     <span class="h3 post_name">
-                            {{$item->user->name}}
+                            {{ $item->user->name }}
                         </span>
                     <div class="float-md-right text-dark h4 mt-5">
-                        Создано: {{$item->created_at}}
+                        Создано: {{ $item->created_at }}
                     </div>
                 </div>
-                <div class="mt-4 "><p class="article_txt">{!!$item->content_html!!}</p></div>
+                <div class="mt-4 "><p class="article_txt">{!! $item->content_html !!}</p></div>
             </div>
         </div>
     </div>
