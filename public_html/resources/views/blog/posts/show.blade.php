b4b8312ac58fd45526bd29007d0b4f6ed859064f
Руслан <ruslan.valeev18117@gmail.com>
2020-05-15 16:56:47 +0300

    I'm finished "Read Article"

--- public_html/resources/views/blog/posts/show.blade.php
+++ public_html/resources/views/blog/posts/show.blade.php
@@ -6,40 +6,40 @@
     {{--  Верстайте тут, в $item будет модель BlogPost                              --}}
     {{--  Для обращения к полю используйте конструкцию {{ $item->НАЗВАНИЕ-ПОЛЯ }}   --}}
     {{--  Ниже вывод всех полей модели через дебагер                                --}}
+    {{-- TODO: Исправить этот гениальный слайдер в одну картинку в что-то другое   --}}
+    {{-- TODO: Сделать 'это' адаптивным для разных устройств  --}}
 
 
 
     <div class="carousel intro_read" data-ride="carousel">
-        <ol class="carousel-indicators">
-
-        </ol>
+        <ol class="carousel-indicators"></ol>
         <div class="carousel-inner">
             <div class="carousel-item active ">
                 <img src="{{asset($item->image)}}" class="d-block w-100  intro_read bg-secondary">
                 <div class="carousel-caption  d-md-block arrow-7">
-                    <span><h1 class="post_name"> {{ $item->title }}</h1></span>
-
-                    <a href="#article_user"><div class="arrow arrow-bottom"></div></a>
-
-
+                    <span>
+                        <h1 class="post_name"> {{ $item->title }}</h1>
+                    </span>
+                    <a href="#article_user">
+                        <div class="arrow arrow-bottom"></div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
     <div class="container">
         <div class="row">
-
             <div class="col-12">
-
                 <div>
-                    <span id="article_user"> <img class="img-fluid article_avatar"
-                                                  src="{{ asset($item->user->avatar) }}" alt=""></span>
+                    <span id="article_user">
+                        <img class="img-fluid article_avatar" src="{{ asset($item->user->avatar) }}" alt="">
+                    </span>
                     <span class="h3 post_name">
                             {{$item->user->name}}
                         </span>
                     <div class="float-md-right text-dark h4 mt-5">
-                            Создано: {{$item->created_at}}
-                        </div>
+                        Создано: {{$item->created_at}}
+                    </div>
                 </div>
 
                 <div class="mt-4 "><p class="article_txt">{!!$item->content_html!!}</p></div>
@@ -47,13 +47,4 @@
             </div>
         </div>
     </div>
-
-
-
-
-
-    {{--    @dd($item->getAttributes())--}}
-
-
-
 @endsection
