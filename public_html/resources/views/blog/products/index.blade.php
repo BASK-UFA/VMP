350f857a387a2b4e2faccaed45b291d7f290e95b
Руслан <ruslan.valeev18117@gmail.com>
2020-05-21 23:19:17 +0300

    Our Work

--- public_html/resources/views/blog/products/index.blade.php
+++ public_html/resources/views/blog/products/index.blade.php
@@ -69,7 +69,7 @@
 
             <div>
                 <div class="col-sm">
-                    Одна из трёх колонок
+                    <img src="{{asset('images/avatar.png')}}" alt="">
                 </div>
                 <div class="col-sm">
                     Одна из трёх колонок
@@ -98,10 +98,50 @@
                 </div>
                 <div class="col-sm">
                     Одна из трёх колонок
+                </div>
+            </div>
+            <div class="col-md-9 m-auto pt-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="pt-5 col-md-12">
+                            <h5>LET’S GET IN TOUCH</h5>
+                            <h1 class="Oswald work-text">
+                                <div>CONTACT DETAILS</div>
+
+                            </h1>
+                        </div>
+                        <div class="col pt-5 pl-4 ">
+                            <p>Our team of 1,200 employees hails from every craft and expertise in the field, allowing
+                                us to
+                                combine innovative construction methods and accountable project management to get the
+                                job
+                                done, and to get it done right. To do this, we work closely with architects, engineers,
+                                subcontractors, and clients at every stage of the process. </p>
+                        </div>
+                        <div class="col pt-5 pl-4 ">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Ваше имя"
+                                       aria-label="Имя пользователя" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Ваш E-mail"
+                                       aria-label="Имя получателя" aria-describedby="basic-addon2">
+                            </div>
+                                <div class="input-group">
+                                    <textarea class="form-control" placeholder="Ваше сообщение" aria-label="With textarea"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    <div class="col-md-12 ">
+                        <button type="button" class="btn btn-outline-warning m-auto">Предупреждение</button>
+                    </div>
+
+
                 </div>
             </div>
         </div>
     </div>
-        @dd($data)
+    {{--        @dd($data)--}}
 
 @endsection
