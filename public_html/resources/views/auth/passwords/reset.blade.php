e408240b72768f96b288eca19b577132dfef8c75
Руслан <ruslan.valeev18117@gmail.com>
2020-05-26 02:21:32 +0300

    ALL

--- public_html/resources/views/auth/passwords/reset.blade.php
+++ public_html/resources/views/auth/passwords/reset.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center ">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card ">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
